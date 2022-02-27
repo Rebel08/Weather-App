d67783eaf9d4b90ee496aa16a97d0a6e9977a9d3
Piyush <57295119+Rebel08@users.noreply.github.com>
2022-02-27 10:34:24 +0530

    remove date error

--- src/dateBuilder.js
+++ src/dateBuilder.js
@@ -3,7 +3,7 @@ export const dateBuilder = (d) => {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
-    let date = d.getDay();
+    let date = d.getData();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
